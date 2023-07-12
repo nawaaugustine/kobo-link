44f4f30b272bacc33e64a559b1f356eca109bf86
Augustine Nawa <nawaa@unhcr.org>
2023-07-12 13:53:14 +0700

    Added try block

--- api/handler.py
+++ api/handler.py
@@ -3,37 +3,41 @@ from urllib.parse import urlparse, parse_qs
 import requests
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
-        # Parse parameters from URL
-        params = parse_qs(urlparse(self.path).query)
-        uid = params.get('uid')[0]
-        id = params.get('id')[0]
+        try:
+            # Parse parameters from URL
+            params = parse_qs(urlparse(self.path).query)
+            uid = params.get('uid')[0]
+            id = params.get('id')[0]
 
-        # Your base URL, change if needed
-        base_url = "https://kobo.unhcr.org"
+            # Your base URL, change if needed
+            base_url = "https://kobo.unhcr.org"
 
-        # Construct the URL
-        url = f"{base_url}/api/v2/assets/{uid}/data/{id}/enketo/edit/?return_url=false"
+            # Construct the URL
+            url = f"{base_url}/api/v2/assets/{uid}/data/{id}/enketo/edit/?return_url=false"
 
-        # Make a GET request
-        response = requests.get(url)
+            # Make a GET request
+            response = requests.get(url)
 
-        # Ensure the request was successful
-        if response.status_code == 200:
-            # Parse the JSON response
-            json_response = response.json()
+            # Ensure the request was successful
+            if response.status_code == 200:
+                # Parse the JSON response
+                json_response = response.json()
 
-            # Extract the URL
-            returned_url = json_response.get('url')
+                # Extract the URL
+                returned_url = json_response.get('url')
 
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(returned_url.encode())
-        else:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(returned_url.encode())
+            else:
+                self.send_response(500)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(f"Request to KoBo API failed with status code: {response.status_code}".encode())
+        except Exception as e:
             self.send_response(500)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(f"Request failed with status code: {response.status_code}".encode())
-        return
+            self.wfile.write(f"Error in Vercel serverless function: {str(e)}".encode())
