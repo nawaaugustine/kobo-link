031743e0113b87d43d9552eae713ae6521ffef4a
Augustine Nawa <nawaa@unhcr.org>
2023-07-12 16:00:55 +0700

    Changed authentication means to basic

--- api/handler.py
+++ api/handler.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler
 import json
 from urllib.parse import urlparse, parse_qs
 import requests
+from base64 import b64encode
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -15,51 +16,39 @@ class handler(BaseHTTPRequestHandler):
             username = user_data.get('username')
             password = user_data.get('password')
 
-            # Authenticate with Kobo
+            # Create the authentication header
+            auth_header = b64encode(f'{username}:{password}'.encode('utf-8')).decode('utf-8')
+
+            # Parse parameters from URL
+            params = parse_qs(urlparse(self.path).query)
+            uid = params.get('uid')[0]
+            id = params.get('id')[0]
+
+            # Construct the URL for the GET request
             base_url = "https://kobo.unhcr.org"
-            auth_url = f"{base_url}/accounts/login/"
-            auth_response = requests.post(auth_url, data={'username': username, 'password': password})
+            url = f"{base_url}/api/v2/assets/{uid}/data/{id}/enketo/edit/?return_url=false"
 
-            # If authentication was successful
-            if auth_response.status_code == 200:
-                # Get the session ID and CSRF token from the cookies
-                cookies = auth_response.cookies
-                session_id = cookies.get('sessionid')
-                csrf_token = cookies.get('csrftoken')
+            # Make a GET request with the Basic Authentication header
+            headers = {'Authorization': f'Basic {auth_header}'}
+            response = requests.get(url, headers=headers)
 
-                # Parse parameters from URL
-                params = parse_qs(urlparse(self.path).query)
-                uid = params.get('uid')[0]
-                id = params.get('id')[0]
-                
-                # Construct the URL for the GET request
-                url = f"{base_url}/api/v2/assets/{uid}/data/{id}/enketo/edit/?return_url=false"
+            # Ensure the request was successful
+            if response.status_code == 200:
+                # Parse the JSON response
+                json_response = response.json()
 
-                # Make a GET request with the cookies
-                response = requests.get(url, cookies={'sessionid': session_id, 'csrftoken': csrf_token})
+                # Extract the URL
+                returned_url = json_response.get('url')
 
-                # Ensure the request was successful
-                if response.status_code == 200:
-                    # Parse the JSON response
-                    json_response = response.json()
-
-                    # Extract the URL
-                    returned_url = json_response.get('url')
-
-                    self.send_response(200)
-                    self.send_header('Content-type', 'application/json')
-                    self.end_headers()
-                    self.wfile.write(json.dumps({'url': returned_url}).encode())
-                else:
-                    self.send_response(500)
-                    self.send_header('Content-type', 'application/json')
-                    self.end_headers()
-                    self.wfile.write(json.dumps({'error': f"Request to KoBo API failed with status code: {response.status_code}"}).encode())
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({'url': returned_url}).encode())
             else:
                 self.send_response(401)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
-                self.wfile.write(json.dumps({'error': 'Authentication failed'}).encode())
+                self.wfile.write(json.dumps({'error': f"Request to KoBo API failed with status code: {response.status_code}"}).encode())
         except Exception as e:
             self.send_response(500)
             self.send_header('Content-type', 'application/json')
